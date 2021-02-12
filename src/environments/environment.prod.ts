1685124b02cf0589cc2aa2c9a69bdb2470637274
sivakrishnareddy1179 <sivakrishnareddy.p@presidio.com>
2021-02-12 16:13:23 +0530

    Deployable

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  YOUTUBE_API_KEY: "AIzaSyDOgAnNokkunCwN2I7--3230NlZR1KcuUE",
-  lYOUTUBE_API_URL: "https://www.googleapis.com/youtube/v3/search"
-};
